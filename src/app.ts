5049a282a14d9b4e1d611b43fd91d0c4e2ecb99a
zillurclub <zillurdeu@gmail.com>
2023-05-31 19:12:50 +0200

    Feat: Add user route and controller

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,17 @@
 import express, { Application, Request, Response } from 'express'
 import cors from 'cors'
-import { createUser } from './app/modules/users/users.service'
+import usersRouter from './app/modules/users/users.route'
+
 const app: Application = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// Aplication route
+app.use('/api/v1/users', usersRouter)
+
 app.get('/', async (req: Request, res: Response) => {
-  await createUser({
-    id: '999',
-    password: '111',
-    role: 'student',
-  })
   res.send('Hello World!')
 })
 export default app
---
+++ src/app/modules/users/users.route.ts
@@ -0,0 +1,5 @@
+import express from 'express'
+import usersController from './users.controller'
+const router = express.Router()
+router.post('/create-user', usersController.createUser)
+export default router
