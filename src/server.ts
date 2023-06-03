e5ad2a070072eb5c90da414065b89f3fcb452a7a
zillur-rgb <zillurdeu@gmail.com>
2023-06-03 21:01:09 +0200

    chore: add logger and erro info log

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
+import logger from './shared/logger'
 
 async function main() {
   try {
-    await mongoose.connect(config.db as string)
+    await mongoose.connect(config.database_url as string)
 
     app.listen(config.port, () => {
-      console.log(`Application is listening on port ${config.port}`)
+      logger.info(`Application is listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log('error from server: ', error)
+    logger.error('error from server: ', error)
   }
 }
 
