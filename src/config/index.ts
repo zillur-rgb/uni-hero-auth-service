52f4ae912a05130f8409d6521d19d7c9ea5dbecb
zillur-rgb <zillurdeu@gmail.com>
2023-06-04 22:46:17 +0200

    chore: add success and error logger

--- src/config/index.ts
+++ src/config/index.ts
@@ -7,6 +7,6 @@ dotenv.config({ path: path.join(process.cwd(), '.env') })
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
-  database_url: process.env.DATABASE_URL,
+  database_url: process.env.DB_URL,
   default_user_pass: process.env.DEFAULT_USER_PASS,
 }
--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,17 @@
 import mongoose from 'mongoose'
 import app from './app'
 import config from './config/index'
-import logger from './shared/logger'
+import { errorLogger, successLogger } from './shared/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
 
     app.listen(config.port, () => {
-      logger.info(`Application is listening on port ${config.port}`)
+      successLogger.info(`Application is listening on port ${config.port}`)
     })
   } catch (error) {
-    logger.error('error from server: ', error)
+    errorLogger.error('error from server: ', error)
   }
 }
 
