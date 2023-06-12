e93cec93b2ce75509fca59031bb5e24627bbebae
MD Zillur Rahman <zillurdeu@gmail.com>
2023-06-13 00:01:01 +0200

    chore: finish creation of zod error handler

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -1,5 +1,11 @@
 import express from 'express'
 import { UserController } from './user.controller'
+import validateRequests from '../../../middlewares/validateRequest'
+import { userValidation } from './user.validation'
 const router = express.Router()
-router.post('/create-user', UserController.createUser)
+router.post(
+  '/create-user',
+  validateRequests(userValidation.createUserZodSchema),
+  UserController.createUser,
+)
 export const UserRoute = router
--- src/middlewares/globalErrorHandler.ts
+++ src/middlewares/globalErrorHandler.ts
@@ -5,6 +5,8 @@ import { IGenericErrorMessage } from '../types/error.type'
 import handleValidationError from '../errors/handleValidationError'
 import ApiError from '../errors/ApiError'
 import { errorLogger } from '../shared/logger'
+import { ZodError } from 'zod'
+import handleZodError from '../errors/handleZodError'
 
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   // console is enough for us to see the logs in development mode but
@@ -20,6 +22,11 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
   // For validation error
   if (error?.name === 'ValidationError') {
     const simplifiedError = handleValidationError(error)
+    statusCode = simplifiedError.statusCode
+    message = simplifiedError.message
+    errorMessages = simplifiedError.errorMessages
+  } else if (error instanceof ZodError) {
+    const simplifiedError = handleZodError(error)
     statusCode = simplifiedError.statusCode
     message = simplifiedError.message
     errorMessages = simplifiedError.errorMessages
